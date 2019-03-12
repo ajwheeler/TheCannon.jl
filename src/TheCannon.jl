4b65907d6195841712dad0d28783a2f5c302db0a
Adam Wheeler <a.wheeler@columbia.edu>
2019-03-12 12:28:02 -0400

    combine train functions

--- src/TheCannon.jl
+++ src/TheCannon.jl
@@ -92,43 +92,8 @@ Run the training step of The Cannon, i.e. calculate coefficients for each pixel.
  - `labels` contains the labels for each star.  It should be `nstars x nlabels`.
     It will be projected into the quadratic label space before training.
 """
-function train(flux::Matrix{Float64}, ivar::Matrix{Float64}, labels::Matrix{Float64}; 
-               quadratic=true)
-    nstars = size(flux,1)
-    npix = size(flux, 2)
-    labels = project_labels(labels; quadratic=quadratic)
-    nplabels = size(labels, 2)
-    println("$nstars stars, $npix pixels, $nplabels (projected) labels")
-
-    theta = Matrix{Float64}(undef, nplabels, npix)
-    scatters = Vector{Float64}(undef, npix)
-
-    #do linear regression for each pixel
-    for i in 1:npix
-        #calculate theta
-        lT_invcov_l = transpose(labels) * Diagonal(ivar[:, i]) * labels 
-        c = cond(lT_invcov_l)
-        if c > 10^8
-            @warn "condition number is very large ($c)"
-        end
-        lT_invcov_F = transpose(labels) * Diagonal(ivar[:, i]) * flux[:,i]
-        theta[:, i] = lT_invcov_l \ lT_invcov_F
-
-        #calculate optimal scatter
-        function negative_log_likelihood(scatter::Float64) #up to constant
-            χ = labels*theta[:, i] - flux[:, i]
-            Σ = Diagonal(ivar[:, i].^(-1) .+ scatter^2)
-            (0.5*(transpose(χ) * inv(Σ) * χ) + #chi-squared
-             0.5*sum(log.(diag(Σ)))) #log(det(Σ))
-        end
-        fit = optimize(negative_log_likelihood, 0, 1)
-        scatters[i] = fit.minimizer
-    end
-    theta, scatters
-end
-
-function regularized_train(flux::Matrix{Float64}, ivar::Matrix{Float64}, 
-                           labels::Matrix{Float64}, Λ=0.1)
+function train(flux::Matrix{Float64}, ivar::Matrix{Float64}, 
+                           labels::Matrix{Float64}; Λ=10, fastmode=false)
     nstars = size(flux,1)
     npix = size(flux, 2)
     nlabels = size(labels, 2)
@@ -142,34 +107,37 @@ function regularized_train(flux::Matrix{Float64}, ivar::Matrix{Float64},
     #do linear regression for each pixel
     for i in 1:npix
         println("pixel $i")
-        function negative_log_likelihood(coeffscatter::Vector{Float64}) #up to constant
+        function negative_log_likelihood(coeffscatter) #up to constant
             scatter = coeffscatter[end]
             coeffs = coeffscatter[1:end-1]
 
-            χ = labels*theta[:, i] - flux[:, i]
+            χ = labels*coeffs - flux[:, i]
             Σ = Diagonal(ivar[:, i].^(-1) .+ scatter^2)
             (0.5*(transpose(χ) * inv(Σ) * χ) + #chi-squared
-             0.5*sum(log.(diag(Σ))))# + #log(det(Σ))
-            #Λ*sum(abs.(coeffs[6:end]))) #L1 penalty
-            #invσ = (ivar[:, i].^(-1) .+ scatter^2).^(-1)
-            #A2 = (labels*coeffs - flux[:, i]).^2
-            #(0.5*sum(A2.*invσ) - 0.5*sum(log.(invσ)) +  
-            # Λ * sum(abs.(coeffs[6:end] .^ 2)) #regularization term
-            # )
+             0.5*sum(log.(diag(Σ))) + #normalizaion term
+             Λ*sum(abs.(coeffs[4:end]))) #L1 penalty
         end
 
         thetascatter0 = zeros(nplabels+1)
         lT_invcov_l = transpose(labels) * Diagonal(ivar[:, i]) * labels 
         lT_invcov_F = transpose(labels) * Diagonal(ivar[:, i]) * flux[:,i]
+        #lT_invcov_l = transpose(labels) * labels 
+        #lT_invcov_F = transpose(labels) * flux[:,i]
         thetascatter0[1:end-1] = lT_invcov_l \ lT_invcov_F
         thetascatter0[end] = 0.01
 
-        fit = optimize(negative_log_likelihood, thetascatter0, iterations=1000)
-        #print(fit)
+        #lower = vcat(fill(-1., nplabels), [0.])
+        #upper = vcat(fill(2., nplabels), [1.])
+        fit = optimize(negative_log_likelihood, thetascatter0,
+                       iterations=fastmode ? 1000 : 100000)
+        #println(fit)
+        if ! fit.g_converged
+            @warn "pixel $i not converged"
+        end
         scatters[i] = fit.minimizer[end]
         theta[:, i] = fit.minimizer[1:end-1]
     end
-    theta, abs.(scatters)
+    theta, scatters
 end
 
 function logπ(label::R, p) where R <: Real
